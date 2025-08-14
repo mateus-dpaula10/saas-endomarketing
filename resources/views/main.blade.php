43d75412dc005af68ba2a966afcd62c26bb86522
Mateus De Paula <mateus.dpaula10@gmail.com>
2025-08-14 15:10:20 -0300

    Adiciona btn wpp direcionando

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -109,6 +109,10 @@
         </div>
     </footer>
 
+    <a href="https://wa.me/5511962544354?text=Olá! Gostaria de mais informações sobre o sistema." target="_blank" id="btnWpp">
+        <i class="fa-brands fa-whatsapp"></i>
+    </a>
+
     <script src="{{ asset('js/script.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
